fff1680b188824bfd28d752144c052c83ec0e899
R Jacob <rizzy@live.com.sg>
2025-07-26 13:44:54 +0800

    seed user and query data

--- src/benchmark-queries.ts
+++ src/benchmark-queries.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from "fs";
-import { PrismaClient, Status, User } from "../generated/prisma/client";
+import { PrismaClient, Status } from "../generated/prisma/client";
 import { faker } from "@faker-js/faker";
 import path from "path";
 
@@ -16,46 +16,86 @@ const QUERIES_TO_ADD = 500;
 
 const prisma = new PrismaClient();
 
-type CreateUserData = {
+type CreateUserDataType = {
   fullName: string;
   contact: string;
   email: string;
 };
 
-const seedUsers = async (rows: number): Promise<User[]> => {
-  const data: CreateUserData[] = [];
+type idType = {
+  id: number;
+};
+
+const seedUsers = async (rows: number): Promise<{ id: number }[]> => {
+  const data: CreateUserDataType[] = [];
   for (let i = 0; i < rows; i++) {
     const firstName = faker.person.firstName();
     const lastName = faker.person.lastName();
     data.push({
       fullName: faker.person.fullName({ firstName, lastName }),
-      contact: faker.helpers.fromRegExp(/[89]\d{7}/),
+      contact: faker.phone.number({ style: "international" }),
       email: faker.internet.email({ firstName, lastName }),
     });
   }
   return prisma.user.createManyAndReturn({
     data,
+    select: {
+      id: true,
+    },
   });
 };
-type CreateQueryData = {
+type CreateQueryDataType = {
   userId: number;
-  assigneeId: number;
+  assigneeId?: number;
   content: string;
   status: Status;
 };
 
-const seedQueries = async (rows: number, users: User[]) => {
+const seedQueries = async (rows: number, userIds: { id: number }[]) => {
+  const assignedRatio = 0.6;
+  const assignedValues = [
+    { value: true, weight: assignedRatio },
+    { value: false, weight: 1 - assignedRatio },
+  ];
+
   const canQueryRatio = 0.8;
   const canQueryCutoffIndex = Math.round(canQueryRatio * USERS_TO_SEED);
-  const canQuery = users.slice(0, canQueryCutoffIndex);
-  const canBeAssigned = users.slice(canQueryCutoffIndex, users.length - 1);
+  const canQuery = userIds.slice(0, canQueryCutoffIndex).map(({ id }) => id);
+  const canBeAssigned = userIds
+    .slice(canQueryCutoffIndex, userIds.length - 1)
+    .map(({ id }) => id);
 
-  const data: CreateQueryData[] = [];
+  const assignedStatusValues = Object.values(Status).filter(
+    (s) => s !== Status.unassigned
+  );
 
+  const data: CreateQueryDataType[] = [];
+
+  let assigned = 0;
+  let unassigned = 0;
+
+  for (let i = 0; i < rows; i++) {
+    const isAssigned = faker.helpers.weightedArrayElement(assignedValues);
+
+    isAssigned ? assigned++ : unassigned++;
+
+    data.push({
+      userId: faker.helpers.arrayElement(canQuery),
+      assigneeId: isAssigned
+        ? faker.helpers.arrayElement(canBeAssigned)
+        : undefined,
+      content: faker.word.words({ count: { min: 150, max: 200 } }),
+      status: isAssigned
+        ? faker.helpers.arrayElement(assignedStatusValues)
+        : Status.unassigned,
+    });
+  }
+
+  console.log(
+    `Seeding ${assigned} assigned and ${unassigned} unassigned queries`
+  );
   return prisma.query.createMany({ data });
 };
-
-const benchmarkUserQuery = async () => {};
 
 const main = async () => {
   prisma.$connect();
@@ -64,7 +104,8 @@ const main = async () => {
     2. Time taken to retrieve queries by user (queried by)
     3. Time taken to insert ${QUERIES_TO_ADD} new queries`);
 
-  await seedData(10_000);
+  const userIds = await seedUsers(USERS_TO_SEED);
+  await seedQueries(QUERIES_TO_SEED, userIds);
 };
 
 main()
